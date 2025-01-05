fd21f6e466f492c79c655b7abb5ed39adf8fdd4f
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-01-05 15:04:05 +0100

    Estilos cambiados en el index.html, actualizando App.tsx y creado el componente Form.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import Form from "./components/Form";
 
 function App() {
- 
-
   return (
     <>
-    <h1 className="text-3xl font-black">Calculadora de Calorías</h1>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-2xl font-bold text-white uppercase">
+            Contador de Calorias
+          </h1>
+        </div>
+      </header>
+     
+<section className="bg-lime-400 py-20 px-5">
+<div className="max-w-4xl mx-auto">
+    <Form />
+  
+</div>
+
+</section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
