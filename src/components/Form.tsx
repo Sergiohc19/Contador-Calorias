41e4c2ab4cc53d28775bb0756a640cb17d9a6022
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-01-05 19:19:05 +0100

    Agregando formulario y estilos

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,53 @@
+import { categories } from "../data/categories";
 
 export default function Form() {
   return (
-    <div>Form</div>
-  )
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">
+          Categoría:
+        </label>
+        <select
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+        >
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">
+          Actividad:
+        </label>
+        <input
+          type="text"
+          id="activity"
+          className="border border-slate-300 p-2 rounded-lg placeholder-black"
+          placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">
+          Calorías:
+        </label>
+        <input
+          id="calories"
+          type="number"
+          className="border border-slate-300 p-2 rounded-lg placeholder-black"
+          placeholder="Calorías Ej. 300 o 500"
+        />
+      </div>
+      <input 
+  type="submit" 
+  className="bg-gray-200 text-black w-full p-2 font-bold uppercase hover:text-white cursor-pointer transition-all duration-1000 hover:bg-black border"
+  value="Guardar"
+/>
+
+
+    </form>
+  );
 }
