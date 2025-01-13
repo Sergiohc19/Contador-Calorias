c8447f9d62aea337c3deb8db40b8618e86e2f2f7
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-01-13 21:04:42 +0100

    Mostrando un mensaje cuando no hay actividades

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -5,68 +5,76 @@ import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
 import { ActivityActions } from "../reducers/activityReducer";
 
 type ActivityListProps = {
-  activities: Activity[];
-  dispatch: Dispatch<ActivityActions>;
+    activities: Activity[];
+    dispatch: Dispatch<ActivityActions>;
 };
 
 export default function ActivityList({
-  activities,
-  dispatch,
+    activities,
+    dispatch,
 }: ActivityListProps) {
-  const categoryName = useMemo(
-    () => (category: Activity["category"]) =>
-      categories.map((cat) => (cat.id === category ? cat.name : "")),
-    [activities]
-  );
+    const categoryName = useMemo(
+        () => (category: Activity["category"]) =>
+            categories.map((cat) => (cat.id === category ? cat.name : "")),
+        [activities]
+    );
 
-  return (
-    <>
-      <h2 className="text-4xl font-bold text-slate-600 text-center">
-        Comida y Actividades
-      </h2>
+    const isEmptyActivity = useMemo(() => activities.length === 0, [activities]);
 
-      {activities.map((activity) => (
-        <div
-          key={activity.id}
-          className="px-5 py-10 bg-white mt-5 flex justify-between "
-        >
-          <div className=" space-y-2 relative">
-            <p
-              className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
+    return (
+        <>
+            <h2 className="text-4xl font-bold text-slate-600 text-center">
+                Comida y Actividades
+            </h2>
+
+            {isEmptyActivity ? (
+                <p className="text-center my-5">No hay actividades aún...</p>
+            ) : (
+                activities.map((activity) => (
+                    <div
+                        key={activity.id}
+                        className="px-5 py-10 bg-white mt-5 flex justify-between "
+                    >
+                        <div className=" space-y-2 relative">
+                            <p
+                                className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
     ${activity.category === 1 ? "bg-lime-400" : "bg-orange-400"} `}
-            >
-              {categoryName(+activity.category)}
-            </p>
-            <p className="text-2xl font-bold pt-5">{activity.name}</p>
-            <p className="font-black text-4xl text-lime-500">
-              {activity.calories} {""}
-              <span>Calorias</span>
-            </p>
-          </div>
+                            >
+                                {categoryName(+activity.category)}
+                            </p>
+                            <p className="text-2xl font-bold pt-5">{activity.name}</p>
+                            <p className="font-black text-4xl text-lime-500">
+                                {activity.calories} {""}
+                                <span>Calorias</span>
+                            </p>
+                        </div>
 
-          <div className="flex gap-5 items-center">
-            <button
-              onClick={() =>
-                dispatch({ type: "set-activeId", payload: { id: activity.id } })
-              }
-            >
-              <PencilSquareIcon className="h-8 w-8 text-amber-400" />
+                        <div className="flex gap-5 items-center">
+                            <button
+                                onClick={() =>
+                                    dispatch({
+                                        type: "set-activeId",
+                                        payload: { id: activity.id },
+                                    })
+                                }
+                            >
+                                <PencilSquareIcon className="h-8 w-8 text-amber-400" />
+                            </button>
 
-            </button>
-
-            <button
-              onClick={() =>
-                dispatch({ type: "delete-activity", payload: { id: activity.id } })
-              }
-            >
-              <TrashIcon   className="h-8 w-8 text-red-500" />
-              
-            </button>
-
-          </div>
-        </div>
-      ))}
-    </>
-  );
+                            <button
+                                onClick={() =>
+                                    dispatch({
+                                        type: "delete-activity",
+                                        payload: { id: activity.id },
+                                    })
+                                }
+                            >
+                                <TrashIcon className="h-8 w-8 text-red-500" />
+                            </button>
+                        </div>
+                    </div>
+                ))
+            )}
+        </>
+    );
 }
-
