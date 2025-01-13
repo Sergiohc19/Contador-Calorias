97a6d817992054193778e20a39d8f1c350101cd8
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-01-13 20:26:42 +0100

    UpdateActivityReducers.tsx

--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -1,39 +1,47 @@
 import { Activity } from "../types";
 
-export type ActivityActions = 
-{ type : "save-activity", payload : { newActivity : Activity } } |
-{ type : "set-activeId", payload : { id : Activity['id'] } } 
+export type ActivityActions =
+  | { type: "save-activity"; payload: { newActivity: Activity } }
+  | { type: "set-activeId"; payload: { id: Activity["id"] } };
 
 export type ActivityState = {
-    activities : Activity[],
-    activeID: Activity['id']
-}
+  activities: Activity[];
+  activeID: Activity["id"];
+};
 
+export const inicialState: ActivityState = {
+  activities: [],
+  activeID: "",
+};
 
-export const inicialState : ActivityState = {
-    activities : [],
-    activeID: ''
-}
- 
 export const activityReducer = (
-    state : ActivityState = inicialState,
-    action : ActivityActions
-) =>  {
-    
-    if (action.type === "save-activity") {
-        // Este código maneja la lógica para actualizar el state
-       
-        return {
-            ...state,
-            activities : [...state.activities, action.payload.newActivity]
-        }
+  state: ActivityState = inicialState,
+  action: ActivityActions
+) => {
+  if (action.type === "save-activity") {
+    // Este código maneja la lógica para actualizar el state
+
+    let updateActivities: Activity[] = [];
+    if (state.activeID) {
+      updateActivities = state.activities.map((activity) =>
+        activity.id === state.activeID ? action.payload.newActivity : activity
+      );
+    } else {
+      updateActivities = [...state.activities, action.payload.newActivity];
     }
 
-    if (action.type === "set-activeId") {
-        return {
-            ...state,
-            activeID: action.payload.id
-        }
-    }
-    return state
-}
+    return {
+      ...state,
+      activities: updateActivities,
+      activeId: "",
+    };
+  }
+
+  if (action.type === "set-activeId") {
+    return {
+      ...state,
+      activeID: action.payload.id,
+    };
+  }
+  return state;
+};
