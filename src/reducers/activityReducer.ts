dffc3f6cbbba818e9cc4d696c1621bcd0dea9469
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-01-13 20:32:03 +0100

    Creando la acción para eliminar actividades

--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -1,47 +1,56 @@
 import { Activity } from "../types";
 
 export type ActivityActions =
-  | { type: "save-activity"; payload: { newActivity: Activity } }
-  | { type: "set-activeId"; payload: { id: Activity["id"] } };
+    { type: "save-activity"; payload: { newActivity: Activity } } |
+    { type: "set-activeId"; payload: { id: Activity["id"] } } |
+    { type: "delete-activity"; payload: { id: Activity["id"] } }
 
 export type ActivityState = {
-  activities: Activity[];
-  activeID: Activity["id"];
+    activities: Activity[];
+    activeID: Activity["id"];
 };
 
 export const inicialState: ActivityState = {
-  activities: [],
-  activeID: "",
+    activities: [],
+    activeID: "",
 };
 
 export const activityReducer = (
-  state: ActivityState = inicialState,
-  action: ActivityActions
+    state: ActivityState = inicialState,
+    action: ActivityActions
 ) => {
-  if (action.type === "save-activity") {
-    // Este código maneja la lógica para actualizar el state
+    if (action.type === "save-activity") {
+        // Este código maneja la lógica para actualizar el state
 
-    let updateActivities: Activity[] = [];
-    if (state.activeID) {
-      updateActivities = state.activities.map((activity) =>
-        activity.id === state.activeID ? action.payload.newActivity : activity
-      );
-    } else {
-      updateActivities = [...state.activities, action.payload.newActivity];
+        let updateActivities: Activity[] = [];
+        if (state.activeID) {
+            updateActivities = state.activities.map((activity) =>
+                activity.id === state.activeID ? action.payload.newActivity : activity
+            );
+        } else {
+            updateActivities = [...state.activities, action.payload.newActivity];
+        }
+
+        return {
+            ...state,
+            activities: updateActivities,
+            activeId: "",
+        };
     }
 
-    return {
-      ...state,
-      activities: updateActivities,
-      activeId: "",
-    };
-  }
+    if (action.type === "set-activeId") {
+        return {
+            ...state,
+            activeID: action.payload.id,
+        };
+    }
 
-  if (action.type === "set-activeId") {
-    return {
-      ...state,
-      activeID: action.payload.id,
-    };
-  }
-  return state;
+    if (action.type === "delete-activity") {
+        return { 
+            ...state,
+            activities: state.activities.filter( activity => activity.id !== action.payload.id)
+        }
+    }
+
+    return state;
 };
