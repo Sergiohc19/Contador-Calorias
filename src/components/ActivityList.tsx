4c0442741359e03928f2e17f63f1b92949eb7110
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-01-13 19:07:23 +0100

    Agregando icono para editar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,6 +1,7 @@
 import { Activity } from "../types";
 import { categories } from "../data/categories";
 import { useMemo } from "react";
+import { PencilSquareIcon } from '@heroicons/react/24/solid'
 
 type ActivityListProps = {
   activities: Activity[];
@@ -35,7 +36,10 @@ export default function ActivityList({ activities }: ActivityListProps) {
 </p>
      </div>
 
-     <div>
+     <div className="flex gap-5 items-center">
+        <button>
+            <PencilSquareIcon className="h-8 w-8 text-amber-400" />
+        </button>
 
      </div>
 
