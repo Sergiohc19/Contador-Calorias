c2639d5bd9cedb90cab7fc5986dac380cce4df3f
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-01-14 01:41:41 +0100

    Mejorando version movile

--- src/App.tsx
+++ src/App.tsx
@@ -18,30 +18,30 @@ function App() {
     <>
       <header className="bg-lime-600 py-3">
         <div className="max-w-4xl mx-auto flex justify-between items-center px-4 sm:px-6">
-          <h1 className="text-center text-2xl font-bold text-white uppercase sm:text-3xl">
+          <h1 className="text-center text-xl sm:text-3xl font-bold text-white uppercase">
             Contador de Calorias
           </h1>
 
           <div className="flex items-center gap-2">
-            <h3 className="text-xl font-black text-black sm:text-2xl">Reiniciar App</h3>
+            <h3 className="text-lg sm:text-xl font-black text-black">Reiniciar App</h3>
             <button
               className="bg-black cursor-pointer rounded-full flex items-center p-1 disabled:opacity-30"
               disabled={!canRestartApp}
               onClick={() => dispatch({ type: "restart-app" })}
             >
-              <PowerIcon className="h-8 w-8 text-gray-500 scale-100 hover:text-green-400 transition-colors duration-100 hover:scale-105 sm:h-9 sm:w-9" />
+              <PowerIcon className="h-7 w-7 sm:h-8 sm:w-8 text-gray-500 scale-100 hover:text-green-400 transition-colors duration-100 hover:scale-105" />
             </button>
           </div>
         </div>
       </header>
 
-      <section className="py-16 px-5 bg-contain bg-center bg-lime-500">
+      <section className="py-12 sm:py-16 px-5 bg-contain bg-center bg-lime-500">
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} state={state} />
         </div>
       </section>
 
-      <section className="bg-gray-800 py-10 px-5 sm:px-10">
+      <section className="bg-gray-800 py-8 sm:py-10 px-5 sm:px-10">
         <div className="max-w-4xl mx-auto">
           <CalorieTracker activities={state.activities} />
         </div>
